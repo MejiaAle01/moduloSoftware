334a86584461a3b576307d91257e55c4ae511737
Alejandro Mejía <alemaniajandro@hotmail.com>
2024-08-19 15:15:22 -0600

    Archivo compartido
    Compartir archivo
    
    Co-Authored-By: Oscarbarrera12 <178835481+oscarbarrera12@users.noreply.github.com>
    Co-Authored-By: Lewis2801 <178226781+lewis2801@users.noreply.github.com>
    Co-Authored-By: jesus-reyes2008 <178522416+jesus-reyes2008@users.noreply.github.com>

---
+++ 05-input.py
@@ -0,0 +1,11 @@
+# Input = Entradas por teclado
+
+nombre = input("Ingrese su nombre: ");
+
+print("Hola: ", nombre);
+
+# Crea un mini programa en Python en el que se use input de su nombre
+# Y compara su edad +18 imprime "Eres mayor de edad", sino "Eres menor"
+
+persona = input("Ingrese su nombre: ");
+edad = input("Ingrese su edad: ");
