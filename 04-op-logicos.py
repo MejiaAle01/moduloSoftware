6153998fef71c505c8418d32d549de021faf70f2
Alejandro Mejía <alemaniajandro@hotmail.com>
2024-08-19 14:24:59 -0600

    Nuevo archivo
    Segundo archivo que subo

---
+++ 04-op-logicos.py
@@ -0,0 +1,15 @@
+# Operadores Lógicos
+num1 = 10;
+num2 = 2.5;
+texto = "Hola";
+
+# AND = Y
+print(num1 > num2 and num1 != num2);
+
+# OR = O
+print(texto > num2 or num1 != texto);
+
+# NOT = NEGACIÓN
+print(not (num1 > num2));
+# not(FALSE)
+# TRUE
