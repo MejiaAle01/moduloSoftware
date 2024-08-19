ac79a2cb71f4ce82c3151cd954a01aeeb5ce5b21
Alejandro Mejía <alemaniajandro@hotmail.com>
2024-08-19 14:16:30 -0600

    Inicio modulo
    Se agregan 3 archivos al repositorio

---
+++ 02-operadores.py
@@ -0,0 +1,22 @@
+# Operadores Aritméticos
+
+# 1: Suma
+print("Suma: ", 5 + 10);
+
+# Alternativas
+print("Suma N Números: ", 5 + 10 + 15);
+
+# 2: Resta
+print("Resta: ", 5 - 15);
+
+# 3: Multiplicación
+print("Multiplicación: ", 3 * 5);
+
+# 4: División
+print("División: ", 15 / 5);
+
+# 5: Potencia
+print("Potencia: ", 2 ** 3);
+
+# 6: MODULO
+print("Modulo: ", 10 % 3);
---
+++ 03-comparacion.py
@@ -0,0 +1,23 @@
+# Operadores de comparación
+
+num1 = 10;
+num2 = 2.5;
+texto = "Hola";
+
+# Mayor que
+print(num1 > num2);
+
+# Menor que
+print(num1 < num2);
+
+# Menor o igual que
+print(num1 <= num2);
+
+# Mayor o igual que
+print(num1 >= num2);
+
+# Igualación
+print(num1 == num2);
+
+# Diferente
+print(num1 != texto);
